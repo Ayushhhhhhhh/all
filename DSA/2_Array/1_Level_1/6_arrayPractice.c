f3d7ed9cc765d1eda83076ad15a37bc551a39e59
Ayushhhhhhhh <80603035+Ayushhhhhhhh@users.noreply.github.com>
2021-10-05 22:50:17 +0530

    working check

--- DSA/2_Array/1_Level_1/6_arrayPractice.c
+++ DSA/2_Array/1_Level_1/6_arrayPractice.c
@@ -16,8 +16,8 @@ int main()
         printf("%d\n", arr[i]);
     }
 
-    //Searching 
-
+    //Searching
+    /*
     int key,flag=0;
     printf("Enter element to search: ");
     scanf("%d",&key);
@@ -31,56 +31,56 @@ int main()
         printf("Element found \n");
     else
         printf("Element not found \n");
+*/
 
-/*
-   //Insertion
-    int position,element;
+    //Insertion
+    int position, element;
     printf("Enter element and the position you want it to insert : \n");
-    scanf("%d %d",&position,&element);
-    for(int i = n-1;i>position-1;i--)
+    scanf("%d %d", &position, &element);
+    for (int i = n - 1; i > position; i--)
     {
-        arr[i+1] = arr[i];
+        arr[i + 1] = arr[i];
     }
-    arr[position-1] = element;
+    arr[position - 1] = element;
     n++;
 
     printf("New array after insersion : \n");
-    for(int i = 0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d\n",arr[i]);
+        printf("%d\n", arr[i]);
     }
-
+    /*
     //Deletion
 
     int position;
     printf("Enter the position of elements you want delete : \n");
-    scanf("%d",&position);
+    scanf("%d", &position);
 
-    for(int i = position-1;i<n;i++)
+    for (int i = position - 1; i < n; i++)
     {
-        arr[i] = arr[i+1];
+        arr[i] = arr[i + 1];
     }
-    n--;f\
+    n--;
+    f
 
-    printf("New array after Deletion : \n");
-    for(int i = 0;i<n;i++)
+        printf("New array after Deletion : \n");
+    for (int i = 0; i < n; i++)
     {
-        printf("%d\n",arr[i]);
+        printf("%d\n", arr[i]);
     }
-  
- // updation
-    int position,element;
+
+    // updation
+    int position, element;
     printf("Enter the element and  position of element you want update : \n");
-    scanf("%d%d",&element,&position);
-    
-    arr[position-1] = element;
-    
+    scanf("%d%d", &element, &position);
 
+    arr[position - 1] = element;
+*/
     printf("New array after Updation : \n");
-    for(int i = 0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d\n",arr[i]);
+        printf("%d\n", arr[i]);
     }
-    */
+
     return 0;
 }
