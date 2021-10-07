b8a764c7b33c15b938b15fff6b1beb2e81e70993
Ayushhhhhhhh <80603035+Ayushhhhhhhh@users.noreply.github.com>
2021-10-07 18:17:19 +0530

    struct

--- DSA/4_Struct/1.c
+++ DSA/4_Struct/1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 struct Student
 {
-    char name;
+    char name[30];
     int rollno;
     int dob;
-    char address;
+    char address[100];
 };
 struct Person
 {
@@ -15,7 +15,19 @@ struct Person
 
 int main()
 {
-    struct Student g1 = {.rollno = 24};
-    printf("%d", g1.rollno);
+    struct Student g1 = {"Ayush Singhal", 24, 041102, "SKIT Jaipur"};
+    struct Student g2 = {"Harshit Sen", 42, 28121997, "Room no :18"};
+    printf("Student information :\n");
+    printf("%s\n", g1.name);
+    printf("%d\n", g1.rollno);
+    printf("%d\n", g1.dob);
+    printf("%s\n", g1.address);
+
+    printf("Student information :\n");
+    printf("%s\n", g2.name);
+    printf("%d\n", g2.rollno);
+    printf("%d\n", g2.dob);
+    printf("%s\n", g2.address);
+
     return 0;
 }
