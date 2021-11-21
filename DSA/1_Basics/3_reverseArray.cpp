f74762778f272d34ee5e80ff25f38963d03762e9
Ayushhhhhhhh <80603035+Ayushhhhhhhh@users.noreply.github.com>
2021-11-22 00:26:12 +0530

    Minor update 1.409

---
+++ DSA/1_Basics/3_reverseArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of array :\t";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array :\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0, j = n - 1; i < j; i++, j--)
+    {
+        int tmp;
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+    return 0;
+}
